2b0d66fa18559bbc5d937bd09ab245c2979c159e
Nick Campbell <campbellnick004@gmail.com>
2025-12-06 22:33:14 -0800

    Added root redirect (replace with home page eventually), issue with previous commit was I forgot to puch

--- proteinSearch/urls.py
+++ proteinSearch/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-
+from django.shortcuts import redirect
 from . import views
 
+def redirect_to_products(request):
+    return redirect('product_list')
+
 urlpatterns = [
+    path('', redirect_to_products),  # root redirects to /products/
     path('products/', views.product_list, name='product_list'),
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
     path('products/<int:product_id>/review/', views.submit_review, name='submit_review'),
